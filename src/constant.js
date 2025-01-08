1b7720dfafe09667d1d84aa30300bb71eff6750b
johny <johny@upnode.org>
2025-01-08 10:33:37 +0700

    refactor

--- src/constant.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EVENT_SUBMIT_VERIFICATION = void 0;
-exports.EVENT_SUBMIT_VERIFICATION = "submitVerification";
--- src/env.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MAX_BEHIND_BLOCKS = exports.WALLET_VERIFIER_ADDRESS = exports.PRIVATE_KEY = exports.RPC_URL = exports.NETWORK = void 0;
-exports.NETWORK = process.env["NETWORK"] || "optimism-sepolia";
-exports.RPC_URL = process.env["RPC_URL"] || "";
-exports.PRIVATE_KEY = process.env["PRIVATE_KEY"] || "";
-exports.WALLET_VERIFIER_ADDRESS = process.env["WALLET_VERIFIER_ADDRESS"] || "";
-exports.MAX_BEHIND_BLOCKS = parseInt(process.env["MAX_BEHIND_BLOCKS"] || "10");
